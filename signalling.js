0ed98016bbc49fcd44e58ebe6e8a5ec7f28f1344
shinpython <shinya.k.python@gmail.com>
2021-01-04 17:40:40 +0900

    ganecoさんの３　signalling.jsの追加

---
+++ signalling.js
@@ -0,0 +1,27 @@
+// Node.jsをインストールして、node signalling.js を行う。
+
+"use strict";
+ 
+let WebSocketServer = require('ws').Server;
+let port = 3001;
+let wsServer = new WebSocketServer({ port: port });
+console.log('websocket server start. port=' + port);
+ 
+wsServer.on('connection', function(ws) {
+  console.log('-- websocket connected --');
+  ws.on('message', function(message) {
+    wsServer.clients.forEach(function each(client) {
+      if (isSame(ws, client)) {
+        console.log('- skip sender -');
+      }
+      else {
+        client.send(message);
+      }
+    });
+  });
+});
+ 
+function isSame(ws1, ws2) {
+  // -- compare object --
+  return (ws1 === ws2);     
+}
